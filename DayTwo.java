96facce9fd65febd3a9a473afa3cc108e94619d0
adhit612 <adhit612@gmail.com>
2018-11-29 08:28:20 -0500

    Little bit of first method
    DId a little work, still errors.

--- DayTwo.java
+++ DayTwo.java
@@ -1,4 +1,13 @@
+import java.util.ArrayList;
+
 public class DayTwo{
+	public static int convertToArrayList(int x){
+		ArrayList <Integer> list = new ArrayList <>();
+		list.add(x%10);
+		for(int i = 0; i < list.size(); i ++)
+			return list.get(i);
+
+	}
 	public static void main(String [] args){
 
 	System.out.println("It's Day Two");
